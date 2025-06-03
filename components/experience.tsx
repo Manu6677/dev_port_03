2f0d9b5c3bb1fde32ad0a9197d42b03ebf5cc94f
manu <manu.panjoria666@gmail.com>
2025-06-03 12:08:50 +0530

    port_03

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,42 +1,31 @@
-"use client"
+"use client";
 
-import { motion } from "framer-motion"
-import { Badge } from "@/components/ui/badge"
+import { motion } from "framer-motion";
+import { Badge } from "@/components/ui/badge";
 
 const experiences = [
   {
-    title: "Frontend Developer Intern",
-    company: "WhatBytes",
-    period: "Jan 2025 - Present",
+    title: "Software Application Developer",
+    company: "Rimes",
+    period: "May 2024 - Present",
     responsibilities: [
       "Worked on a live project called Cura Care (curacare.in)",
       "Implemented website analytics to track user interactions using PostHog, Google Analytics, and Facebook Pixel",
       "Developed dynamic slot management system based on bookings",
     ],
-    skills: ["Next.js","ReactJs", "JavaScript", "TailwindCSS"],
+    skills: ["Next.js", "ReactJs", "JavaScript", "TailwindCSS"],
   },
   {
-    title: "Web Developer",
-    company: "Ministry of Electronics and Information Technology",
-    period: "Mar 2024 – Jun 2024",
+    title: "Software Engineer",
+    company: "Excellence",
+    period: "Oct 2023 – May 2025",
     responsibilities: [
       "Developed a CMS dashboard for eDeskshaam, enhancing UI/UX and improving user interaction",
       "Designed and implemented responsive 'Important Links' and 'Events' pages using React.js",
     ],
     skills: ["React", "JavaScript", "UI/UX", "Responsive Design"],
   },
-  {
-    title: "Web Developer",
-    company: "Ministry of Electronics and Information Technology",
-    period: "Mar 2023 – Dec 2023",
-    responsibilities: [
-      "Created four interactive educational games (Identify Fruits, Animals, Vehicles, Body Parts)",
-      "Developed user-friendly homepage for educational games and collaborated with UX designers",
-      "Worked closely with back-end developers to ensure seamless integration and cohesive user experience",
-    ],
-    skills: ["React", "JavaScript", "UI/UX", "Responsive Design"],
-  },
-]
+];
 
 export default function Experience() {
   return (
@@ -87,5 +76,5 @@ export default function Experience() {
         </div>
       </motion.div>
     </section>
-  )
+  );
 }
